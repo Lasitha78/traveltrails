42182173487c339fdbce18d2f93b974189c26027
lasitha gunawardena <lasitha@gmail.com>
2025-03-03 21:14:43 +0530

    Initial commit

---
+++ src/main/java/com/cosmoforge/traveltrails/TravelTrailsApplication.java
@@ -0,0 +1,13 @@
+package com.cosmoforge.traveltrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTrailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmoforge/traveltrails/TravelTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmoforge.traveltrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
